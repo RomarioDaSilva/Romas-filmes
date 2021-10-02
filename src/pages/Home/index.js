5ee94b5b938e6847c6f133065e6054c71687f856
RomárioDaSilva <romario2656@gmail.com>
2021-10-02 18:58:53 -0300

    FinalizandoSearch

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -15,18 +15,17 @@ import Header from "../../components/Header";
 import SliderItem from "../../components/SliderItem";
 import { Feather } from "@expo/vector-icons";
 import api, { key } from "../../services/api";
-import { getlistmovies, randomBanner } from '../../utils/movie';
-import { useNavigation } from "@react-navigation/native"; 
+import { getlistmovies, randomBanner } from "../../utils/movie";
+import { useNavigation } from "@react-navigation/native";
 
 export default function Home() {
-
-
   const [nowMovies, setNowMovies] = useState([]);
   const [popularMovies, setPopularMovies] = useState([]);
   const [topMovies, setTopMovies] = useState([]);
   const [bannerMovies, setBannerMovies] = useState({});
   const [loading, setLoading] = useState(true);
   const navigation = useNavigation();
+  const [input, setInput] = useState("");
 
   useEffect(() => {
     let isActive = true;
@@ -57,20 +56,21 @@ export default function Home() {
         }),
       ]);
 
-      if(isActive){
+      if (isActive) {
         const nowList = getlistmovies(5, nowData.data.results);
         const popularList = getlistmovies(5, popularData.data.results);
         const topList = getlistmovies(5, topData.data.results);
 
-        setBannerMovies(nowData.data.results[randomBanner(nowData.data.results)])
-  
+        setBannerMovies(
+          nowData.data.results[randomBanner(nowData.data.results)]
+        );
+
         setNowMovies(nowList);
         setPopularMovies(popularList);
         setTopMovies(topList);
-  
+
         setLoading(false);
       }
-
     }
 
     getMovies();
@@ -79,39 +79,53 @@ export default function Home() {
     return () => {
       isActive = false;
       ac.abort();
-    }
-
+    };
   }, []);
 
-  function navigateDetailsPage(item){
-    navigation.navigate('Detail', {id: item.id })
+  function navigateDetailsPage(item) {
+    navigation.navigate("Detail", { id: item.id });
   }
 
-  if(loading){
-    return(
+  function handleSearcMovie() {
+    if (input === "") return;
+
+    navigation.navigate("Search", { name: input });
+    setInput("");
+  }
+
+  if (loading) {
+    return (
       <Container>
         <ActivityIndicator size="large" color="#FFF" />
       </Container>
-    )
+    );
   }
   return (
     <Container>
       <Header title="React Prime" />
 
       <SearchContainer>
-        <Input placeholder="Ex Vingadores" placeholderTextColor="#DDD" />
-        <SearchBtn>
+        <Input
+          placeholder="Ex Vingadores"
+          placeholderTextColor="#DDD"
+          value={input}
+          onChangeText={(text) => setInput(text)}
+        />
+        <SearchBtn onPress={handleSearcMovie}>
           <Feather name="search" size={30} color="#FFF" />
         </SearchBtn>
       </SearchContainer>
 
       <ScrollView showsVerticalScrollIndicator={false}>
         <Title>Em Cartaz</Title>
-        <BannerBtn activeOpacity={0.9} onPress={() => navigateDetailsPage(bannerMovies) }>
+        <BannerBtn
+          activeOpacity={0.9}
+          onPress={() => navigateDetailsPage(bannerMovies)}
+        >
           <Banner
             resizeMethod="resize"
             source={{
-              uri: `https://image.tmdb.org/t/p/original/${bannerMovies.poster_path}` ,
+              uri: `https://image.tmdb.org/t/p/original/${bannerMovies.poster_path}`,
             }}
           />
         </BannerBtn>
@@ -120,8 +134,13 @@ export default function Home() {
           horizontal={true}
           showsHorizontalScrollIndicator={false}
           data={nowMovies}
-          renderItem={({ item }) => <SliderItem data={item} navigatePage={ () => navigateDetailsPage(item)} />}
-          keyExtractor={ (item) => String(item.id) }
+          renderItem={({ item }) => (
+            <SliderItem
+              data={item}
+              navigatePage={() => navigateDetailsPage(item)}
+            />
+          )}
+          keyExtractor={(item) => String(item.id)}
         />
 
         <Title>Populares</Title>
@@ -129,8 +148,13 @@ export default function Home() {
           horizontal={true}
           showsHorizontalScrollIndicator={false}
           data={popularMovies}
-          renderItem={({ item }) => <SliderItem data={item} navigatePage={ () => navigateDetailsPage(item)} />}
-          keyExtractor={ (item) => String(item.id) }
+          renderItem={({ item }) => (
+            <SliderItem
+              data={item}
+              navigatePage={() => navigateDetailsPage(item)}
+            />
+          )}
+          keyExtractor={(item) => String(item.id)}
         />
 
         <Title>Mais Votados</Title>
@@ -138,8 +162,13 @@ export default function Home() {
           horizontal={true}
           showsHorizontalScrollIndicator={false}
           data={topMovies}
-          renderItem={({ item }) => <SliderItem data={item} navigatePage={ () => navigateDetailsPage(item)} />}
-          keyExtractor={ (item) => String(item.id) }
+          renderItem={({ item }) => (
+            <SliderItem
+              data={item}
+              navigatePage={() => navigateDetailsPage(item)}
+            />
+          )}
+          keyExtractor={(item) => String(item.id)}
         />
       </ScrollView>
     </Container>
